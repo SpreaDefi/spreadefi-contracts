c2bb1bfc861ec82b26787022f012ded64fb34e09
gas limit <gas_limit@Ubuntu.myguest.virtualbox.org>
2024-07-06 18:02:31 -0400

    refactored createPosition

--- src/Master.sol
+++ src/Master.sol
@@ -29,71 +29,68 @@ contract Master {
         BASE
     }
 
+    constructor() {}
 
-    constructor() {
-        
+    struct PositionParams {
+        string implementation;
+        address quoteToken;
+        address baseToken;
+        uint256 collateralAmount;
+        uint256 flashLoanAmount;
+        uint256 minTokenOut;
+        uint256 moneyMarketBorrowAmount;
     }
 
-   function createPosition(   
-        string calldata _implementation,
-        address _quoteToken,
-        address _baseToken,
-        uint256 _collateralAmount,
-        uint256 _flashLoanAmount,
-        uint256 _minTokenOut,
-        uint256 _moneyMarketBorrowAmount
-    ) public returns(uint256 tokenId, address proxyAddress) {
-
-        ICentralRegistry.Implementation memory implementation_ = centralRegistry.implementations(_implementation);
+    function createPosition(PositionParams memory params) public returns (uint256 tokenId, address proxyAddress) {
+        ICentralRegistry.Implementation memory implementation_ = centralRegistry.implementations(params.implementation);
         address implementationAddress = implementation_.implementation;
-        IMaster.PositionType positionType_ = implementation_.positionType;
-        IMaster.MarginType marginType_ = implementation_.marginType;
+        ICentralRegistry.PositionType positionType_ = implementation_.positionType;
+        ICentralRegistry.MarginType marginType_ = implementation_.marginType;
 
         if (implementationAddress == address(0)) revert ImplementationNotFound();
 
-        IERC20 quoteToken = IERC20(_quoteToken);
-
-        // create a long position
-        if (positionType_ == IMaster.PositionType.LONG) {  
-
-            // create long position with quote token as margin
-            if (marginType_ == IMaster.MarginType.QUOTE) {
-
-                quoteToken.safeTransferFrom(msg.sender, address(this), _collateralAmount);
-
-                // deploy a new proxy contract, mint a new NFT
-                (tokenId, proxyAddress) = factory.createProxy(msg.sender, implementationAddress, _quoteToken, _baseToken);
-
-                quoteToken.safeIncreaseAllowance(proxyAddress, _collateralAmount);
-
-                // add to the position
-                IProxy(proxyAddress).addToPosition(_collateralAmount, _flashLoanAmount, _minTokenOut, _moneyMarketBorrowAmount);
-            } 
-            // create long position with base token as margin
-            else if (marginType_ == IMaster.MarginType.BASE) {
-
-            }
-            else {
-                revert InvalidMarginType();
-            }
+        if (positionType_ == ICentralRegistry.PositionType.LONG) {  
+            (tokenId, proxyAddress) = _createLongPosition(params, implementationAddress, marginType_);
+        } else if (positionType_ == ICentralRegistry.PositionType.SHORT) {
+        
+        } else {
+            revert InvalidPositionType();
         }
-        // create short position
-        else if (positionType_ == IMaster.PositionType.SHORT) {
+    }
+
+    function _createLongPosition(PositionParams memory params, address implementationAddress, ICentralRegistry.MarginType marginType_) 
+        internal returns (uint256 tokenId, address proxyAddress) 
+    {
+        IERC20 quoteToken = IERC20(params.quoteToken);
+        IERC20 baseToken = IERC20(params.baseToken);
+
+        if (marginType_ == ICentralRegistry.MarginType.QUOTE) {
+            quoteToken.safeTransferFrom(msg.sender, address(this), params.collateralAmount);
+
+            (tokenId, proxyAddress) = factory.createProxy(msg.sender, implementationAddress, params.quoteToken, params.baseToken);
+
+            quoteToken.safeIncreaseAllowance(proxyAddress, params.collateralAmount);
+
+            IProxy(proxyAddress).addToPosition(params.collateralAmount, params.flashLoanAmount, params.minTokenOut, params.moneyMarketBorrowAmount);
+
+        } else if (marginType_ == ICentralRegistry.MarginType.BASE) {
+
+            baseToken.safeTransferFrom(msg.sender, address(this), params.collateralAmount);
+
+            (tokenId, proxyAddress) = factory.createProxy(msg.sender, implementationAddress, params.quoteToken, params.baseToken);
+
+            baseToken.safeIncreaseAllowance(proxyAddress, params.collateralAmount);
+
+            IProxy(proxyAddress).addToPosition(params.collateralAmount, params.flashLoanAmount, params.minTokenOut, params.moneyMarketBorrowAmount);
+            
+        } else {
+            revert InvalidMarginType();
         }
-    
     }
 
+    function addToPosition(uint256 _tokenId) public {}
 
-    function addToPosition(uint256 _tokenId) public {
+    function removeFromPosition(uint256 _tokenId) public {}
 
-    }
-
-    function removeFromPosition(uint256 _tokenId) public {
-
-    }
-
-    function closePosition(uint256 _tokenId) public {
-
-    }
-
+    function closePosition(uint256 _tokenId) public {}
 }
--- src/interfaces/ICentralRegistry.sol
+++ src/interfaces/ICentralRegistry.sol
@@ -2,16 +2,25 @@
 pragma solidity ^0.8.0;
 
 import "../interfaces/ICentralRegistry.sol";
-import "../interfaces/IMaster.sol";
 
 interface ICentralRegistry {
-    function implementations(string calldata) external view returns (ICentralRegistry.Implementation memory);
+    function implementations(string calldata) external view returns (Implementation memory);
 
     struct Implementation {
         address implementation;
-        IMaster.PositionType positionType;
-        IMaster.MarginType marginType;
+        PositionType positionType;
+        MarginType marginType;
     }
 
-    
+    enum PositionType {
+        LONG,
+        SHORT
+    }
+
+    enum MarginType {
+        QUOTE,
+        BASE
+    }
+
+
 }
