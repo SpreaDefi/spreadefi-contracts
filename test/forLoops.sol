a02ed5402c084f3eda1904fc1154d1366a62ba76
gas limit <gas_limit@Ubuntu.myguest.virtualbox.org>
2024-07-11 18:28:02 -0400

    trying to find allowance issue in flashloan mock

---
+++ test/forLoops.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import {DSTest} from "ds-test/test.sol";
+
+contract ForLoops {
+
+
+    function setUp() public {
+
+    }
+
+    function testForLoop() public {
+
+
+        
+
+
+
+
+
+
+
+
+    }
+}
