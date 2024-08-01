30e8b98b33ddf5b05e65aef5f3b11abb363d41cf
vboxuser <gas_limit@Ubuntu.myguest.virtualbox.org>
2024-07-31 23:16:53 -0400

    got close short base to work

--- test/strategy/Short_Base_Odos_ZeroLend/Short_Base_Odos_ZeroLend.t.sol
+++ test/strategy/Short_Base_Odos_ZeroLend/Short_Base_Odos_ZeroLend.t.sol
@@ -32,9 +32,9 @@ contract Using_Proxy_Short_Base_Odos_ZeroLend_Test is Test, IERC721Receiver {
 
     /* %%%%%%%%%%%%%%%% ODOS API VARIABLES %%%%%%%%%%%%%%%% */
 
-    bytes odosAdd = hex"83bd37f90001e5d7c2a44ffddf6b295a15c148167daaaf5cf34f0001176211869ca2b568f2a7d4ee941e073a821ee1ff0801434c04928f800004118576ca028f5c00017D2b63A9ab475397d9c247468803F25Cf6523B76000000014f81992FCe2E1846dD528eC0102e6eE1f61ed3e20000000003010203000a0101010200ff000000000000000000000000000000000000000000586733678b9ac9da43dd7cb83bbb41d23677dfc3e5d7c2a44ffddf6b295a15c148167daaaf5cf34f000000000000000000000000000000000000000000000000";
+    bytes odosAdd = hex"83bd37f90001e5d7c2a44ffddf6b295a15c148167daaaf5cf34f0001176211869ca2b568f2a7d4ee941e073a821ee1ff0801434c04928f80000411442c27028f5c00017D2b63A9ab475397d9c247468803F25Cf6523B76000000014f81992FCe2E1846dD528eC0102e6eE1f61ed3e200000000050103060131955bdd0a010001020000100201010302020a0201040500ff000000f11bb479dc3daffe63989b6b95f6c119225dac28e5d7c2a44ffddf6b295a15c148167daaaf5cf34fdded227d71a096c6b5d87807c1b5c456771aaa941d6cbd5ab95fcc04edde14abfa8d363adf4ead003aab2285ddcddad8edf438c1bab47e1a9d05a9b400000000000000000000000000000000000000000000000000000000";
 
-    bytes odosRemove = hex'83bd37f90001176211869ca2b568f2a7d4ee941e073a821ee1ff0001e5d7c2a44ffddf6b295a15c148167daaaf5cf34f0401f7151007242e0582301d04028f5c00017D2b63A9ab475397d9c247468803F25Cf6523B76000000014f81992FCe2E1846dD528eC0102e6eE1f61ed3e20000000003010203000a0101010201ff000000000000000000000000000000000000000000586733678b9ac9da43dd7cb83bbb41d23677dfc3176211869ca2b568f2a7d4ee941e073a821ee1ff000000000000000000000000000000000000000000000000';
+    bytes odosRemove = hex'83bd37f90001176211869ca2b568f2a7d4ee941e073a821ee1ff0001e5d7c2a44ffddf6b295a15c148167daaaf5cf34f0401ee146007243177b26f1eea028f5c00017D2b63A9ab475397d9c247468803F25Cf6523B76000000014f81992FCe2E1846dD528eC0102e6eE1f61ed3e20000000003010203000a0101010201ff000000000000000000000000000000000000000000586733678b9ac9da43dd7cb83bbb41d23677dfc3176211869ca2b568f2a7d4ee941e073a821ee1ff000000000000000000000000000000000000000000000000';
 
     bytes odosClose = hex'83bd37f90001176211869ca2b568f2a7d4ee941e073a821ee1ff0001e5d7c2a44ffddf6b295a15c148167daaaf5cf34f040c1988c007d97f7655498220028f5c0001d804BA88371A3f00dDaCA03Cbc2b6C47F38105FC000000014f81992FCe2E1846dD528eC0102e6eE1f61ed3e20000000003010203000a0101010201ff000000000000000000000000000000000000000000586733678b9ac9da43dd7cb83bbb41d23677dfc3176211869ca2b568f2a7d4ee941e073a821ee1ff000000000000000000000000000000000000000000000000';
     
@@ -135,7 +135,7 @@ contract Using_Proxy_Short_Base_Odos_ZeroLend_Test is Test, IERC721Receiver {
 
         // lower exposure to eth
         IMaster.PositionParams memory removeParams = IMaster.PositionParams({
-            marginAmount: 32_970000, // usdc price of 0.01 ether
+            marginAmount: 32380000, // usdc price of 0.01 ether
             flashLoanAmount: 0.01 ether,
             pathDefinition: odosRemove
         });
@@ -144,6 +144,38 @@ contract Using_Proxy_Short_Base_Odos_ZeroLend_Test is Test, IERC721Receiver {
             0,
             removeParams
         );
+    }
+
+    function testClose() public {
+        deal(WETHAddress, (address(this)), 1 ether, false);
+
+        IMaster.NewPositionParams memory params = IMaster.NewPositionParams ({
+            implementation: "SHORT_BASE_ODOS_ZEROLEND",
+            quoteToken: USDCAddress,
+            baseToken: WETHAddress
+        });
+
+        IMaster(address(master)).createPosition(params);
+
+        uint256 nftBalance = leveragedNFT.balanceOf(address(this));
+
+        assertEq(nftBalance, 1);
+
+        IMaster.PositionParams memory positionParams = IMaster.PositionParams({
+            marginAmount: 0.03 ether,
+            flashLoanAmount: 0.061 ether,
+            pathDefinition: odosAdd
+        });
+
+        IERC20(WETHAddress).approve(address(master), 0.03 ether);
+
+        IMaster(address(master)).addToPosition(
+            0,
+            positionParams
+        );
+        
+        IMaster(address(master)).closePosition(0, odosClose);
+
     }
 
 
