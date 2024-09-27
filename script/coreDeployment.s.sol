e78c02da7bc92a3bd58f9c5009f3e3f46373512c
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2024-09-27 22:35:15 +0530

    feat: New deployements and minor changes in scripts

--- script/coreDeployment.s.sol
+++ script/coreDeployment.s.sol
@@ -16,7 +16,6 @@ import "src/interfaces/IMaster.sol";
 import "src/interfaces/IERC721Receiver.sol";
 
 contract CoreDeployment is Script, IERC721Receiver {
-
     event debugAddress(string, address);
     event debugUint(string, uint);
 
@@ -39,15 +38,17 @@ contract CoreDeployment is Script, IERC721Receiver {
 
     address PROXY_ADDRESS;
 
-    bytes getUSDC = hex"83bd37f90001e5d7c2a44ffddf6b295a15c148167daaaf5cf34f0001176211869ca2b568f2a7d4ee941e073a821ee1ff080de0b6b3a7640000049045517b028f5c000156c85a254DD12eE8D9C04049a4ab62769Ce9821000000001f39Fd6e51aad88F6F4ce6aB8827279cffFb92266000000000d04050f0119d1f2010801010102011dc727940a0101030200015716e4b61001010104020118a6431108020005020192d901820a020006020001d09128f40a0300070200000a0400080200080a0101090a00040a01010b0c00060a01010d0e00ff00000000000000000000000000000000000000000000000000000000000000005ec5b1e9b1bd5198343abb6e55fb695d2f7bb308e5d7c2a44ffddf6b295a15c148167daaaf5cf34f7077f0cff76077d0ebb335b607db574400510557dded227d71a096c6b5d87807c1b5c456771aaa948aebffb3964ec5cea0915080ddc1aca079583a4d8611456f845293edd3f5788277f00f7c05ccc29168594a53fc98eea213784534f44bcc08248b4e785afda31027c3e6a03c77a113ffc031b564abbf051d6cbd5ab95fcc04edde14abfa8d363adf4ead003aab2285ddcddad8edf438c1bab47e1a9d05a9b4efd5ec2cc043e3bd3c840f7998cc42ee712700baa219439258ca9da29e9cc4ce5596924745e12b93a48e0630b7b9dcb250112143c9d0fe47d26cb1e44af15ec2a0bd43db75dd04e62faa3b8ef36b00d50000000000000000";
+    bytes getUSDC =
+        hex"83bd37f90001e5d7c2a44ffddf6b295a15c148167daaaf5cf34f0001176211869ca2b568f2a7d4ee941e073a821ee1ff080de0b6b3a764000004979f1bdd028f5c000156c85a254DD12eE8D9C04049a4ab62769Ce9821000000001f39Fd6e51aad88F6F4ce6aB8827279cffFb922660000000004010205013218bddc0801010102012abcc34e0a0101030200000901010204ff005ec5b1e9b1bd5198343abb6e55fb695d2f7bb308e5d7c2a44ffddf6b295a15c148167daaaf5cf34f586733678b9ac9da43dd7cb83bbb41d23677dfc3176211869ca2b568f2a7d4ee941e073a821ee1ff00000000000000000000000000000000";
 
     function run() public {
-
         // set up protocol
 
         vm.startBroadcast(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
 
-        centralRegistry = new CentralRegistry(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        centralRegistry = new CentralRegistry(
+            0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+        );
         master = new Master(address(centralRegistry));
         factory = new Factory(address(centralRegistry));
         leveragedNFT = new LeveragedNFT(address(centralRegistry));
@@ -56,30 +57,29 @@ contract CoreDeployment is Script, IERC721Receiver {
         shortQuoteOdosZerolend = new Short_Quote_Odos_Zerolend();
         shortBaseOdosZerolend = new Short_Base_Odos_Zerolend();
 
-
         centralRegistry.addCore("MASTER", address(master));
         centralRegistry.addCore("FACTORY", address(factory));
         centralRegistry.addCore("LEVERAGE_NFT", address(leveragedNFT));
         centralRegistry.addProtocol("ODOS_ROUTER", OdosRouterAddress);
         centralRegistry.addProtocol("ZEROLEND_POOL", zeroLendAddress);
-        centralRegistry.addImplementation("LONG_QUOTE_ODOS_ZEROLEND", address(longQuoteOdosZerolend));
-        centralRegistry.addImplementation("LONG_BASE_ODOS_ZEROLEND", address(longBaseOdosZerolend));
-        centralRegistry.addImplementation("SHORT_QUOTE_ODOS_ZEROLEND", address(shortQuoteOdosZerolend));
-        centralRegistry.addImplementation("SHORT_BASE_ODOS_ZEROLEND", address(shortBaseOdosZerolend));
-
-        // deposit ETH into WETH
-        WETH(WETHAddress).deposit{value: 10 ether}();
-
-        IERC20(WETHAddress).approve(address(OdosRouterAddress), type(uint256).max);
-
-        // aquire quote token using odos
-        OdosRouterAddress.call(getUSDC);
-
-        // approve master to spend tokens
-        IERC20(USDCAddress).approve(address(master), type(uint256).max);
+        centralRegistry.addImplementation(
+            "LONG_QUOTE_ODOS_ZEROLEND",
+            address(longQuoteOdosZerolend)
+        );
+        centralRegistry.addImplementation(
+            "LONG_BASE_ODOS_ZEROLEND",
+            address(longBaseOdosZerolend)
+        );
+        centralRegistry.addImplementation(
+            "SHORT_QUOTE_ODOS_ZEROLEND",
+            address(shortQuoteOdosZerolend)
+        );
+        centralRegistry.addImplementation(
+            "SHORT_BASE_ODOS_ZEROLEND",
+            address(shortBaseOdosZerolend)
+        );
 
         vm.stopBroadcast();
-
     }
 
     function onERC721Received(
@@ -91,7 +91,6 @@ contract CoreDeployment is Script, IERC721Receiver {
         // Here you can add your custom logic if needed
         return this.onERC721Received.selector;
     }
-
 }
 
 interface WETH {
--- src/Proxy.sol
+++ src/Proxy.sol
@@ -5,14 +5,16 @@ pragma solidity ^0.8.0;
 /// @notice This contract delegates all calls to an implementation contract.
 /// @dev This contract uses the EVM's delegatecall to forward calls to the implementation.
 contract Proxy {
-
     /// @notice The address of the implementation contract
     address public immutable implementation;
 
     /// @notice Constructor to set the implementation address
     /// @param _implementation The address of the implementation contract
     constructor(address _implementation) {
-        require(_implementation != address(0), "Invalid implementation address");
+        require(
+            _implementation != address(0),
+            "Invalid implementation address"
+        );
         implementation = _implementation;
     }
 
@@ -21,7 +23,6 @@ contract Proxy {
     /// @param impl The address of the implementation contract
     function _delegate(address impl) internal virtual {
         assembly {
-
             calldatacopy(0, 0, calldatasize())
 
             let result := delegatecall(gas(), impl, 0, calldatasize(), 0, 0)
@@ -29,7 +30,6 @@ contract Proxy {
             returndatacopy(0, 0, returndatasize())
 
             switch result
-
             case 0 {
                 revert(0, returndatasize())
             }
@@ -39,11 +39,9 @@ contract Proxy {
         }
     }
 
-   /// @notice Fallback function to delegate all calls to the implementation contract
+    /// @notice Fallback function to delegate all calls to the implementation contract
     /// @dev This function will catch any call to the contract and forward it to the implementation
     fallback() external payable virtual {
         _delegate(implementation);
-
     }
-    
 }
